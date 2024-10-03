eb7ee5ab0cf9dc26f08eddc0282330282a903629
justkmode <136920389+justkmode@users.noreply.github.com>
2024-10-03 13:39:49 +0200

    Create views.py

---
+++ littlelemon/restaurant/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import BookingForm
+from .models import Menu
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def menu(request):
+    menu_items = Menu.objects.all()  # Fetch all menu items
+    context = {'menu': menu_items}
+    return render(request, 'menu.html', context)
+
+def contact(request):
+    # Logic for handling contact form could go here
+    return render(request, 'contact.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')  # Redirect to a success page
+    context = {'form': form}
+    return render(request, 'book.html', context)
+
+def success(request):
+    return render(request, 'success.html')  # Create a success.html template
+
+def display_menu_items(request, pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+    else:
+        menu_item = None
+    
+    return render(request, 'menu_item.html', {'menu_item': menu_item})
